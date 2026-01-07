a6274d972849b38189d2b7c491a7824fe2ce503b
santanupaikaray23 <santanupaikaray1996@gmail.com>
2026-01-07 14:35:11 +0530

    add some changes

---
+++ Date.js
@@ -0,0 +1,15 @@
+ Date()
+'Wed Jan 07 2026 04:48:08 GMT+0530 (India Standard Time)'
+var a = new Date()
+undefined
+a.getDate()
+7
+a.getMonth()
+0
+a.getDay()
+3
+a.getFullYear()
+2026
+
+>Month start with 0 as Jan and 11 as Dec
+>Day start with 0 as Sunday and 1 as monday
